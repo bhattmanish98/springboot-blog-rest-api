0733f01032e8dd397de754af5c051e8908ca547a
Manish Bhatt <manish.bhatt@lji.io>
2022-01-30 19:55:23 +0530

    Securing Rest APIs with Database Authentication

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,11 +23,15 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // To enable method level security
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired // To remove in-memory password
+    private CustomUserDetailsService customUserDetailsService;
+
     // To encrypt the password
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -38,13 +45,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean // Inorder to make the InMemoryUserDetailsManager Object as Spring Bean
-    protected UserDetailsService userDetailsService() {
-//        Make sure to encode the password otherwise it will throw exceptions like bad credentials
-        UserDetails root = User.builder().username("root").password(passwordEncoder()
-                .encode("root")).roles("USER").build();
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder()
-                .encode("admin")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(root, admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
+
+//    @Override
+//    @Bean // Inorder to make the InMemoryUserDetailsManager Object as Spring Bean
+//    protected UserDetailsService userDetailsService() {
+////        Make sure to encode the password otherwise it will throw exceptions like bad credentials
+//        UserDetails root = User.builder().username("root").password(passwordEncoder()
+//                .encode("root")).roles("USER").build();
+//        UserDetails admin = User.builder().username("admin").password(passwordEncoder()
+//                .encode("admin")).roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(root, admin);
+//    }
 }
--- src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
+++ src/main/java/com/springboot/blog/security/CustomUserDetailsService.java
@@ -15,11 +15,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class customUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
 
-    public customUserDetailsService(UserRepository userRepository) {
+    public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
