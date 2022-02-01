9955b4b39c0bb550d47b50da7ab081e40c6a8e31
Manish Bhatt <manish.bhatt@lji.io>
2022-02-01 16:16:21 +0530

    added JWT token provider

---
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.springboot.blog.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component // To make this class as spring component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override // This method get called whenever an exception is thrown due to unauthorized user trying to access a resource that require authentication
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
---
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.springboot.blog.security;
+
+import com.springboot.blog.exception.BlogAPIException;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration-milliseconds}")
+    private int jwtExpirationTimeInMs;
+
+//    Generate token
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expiryDate = new Date(currentDate.getTime() + jwtExpirationTimeInMs);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.ES512, jwtSecret)
+                .compact();
+        return token;
+    }
+
+    // get username from the token
+    public String getUsernameFromJwt(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    // Validate JWT
+    public boolean validateJwt(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claim string is empty");
+        }
+
+    }
+}
