f8fac1593e041e324d1c7af496a403ec2889b3b4
Manish Bhatt <manish.bhatt@lji.io>
2022-02-01 17:09:57 +0530

    Change in Signin API to return JWT

--- src/main/java/com/springboot/blog/security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -27,7 +27,7 @@ public class JwtTokenProvider {
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.ES512, jwtSecret)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
         return token;
     }
