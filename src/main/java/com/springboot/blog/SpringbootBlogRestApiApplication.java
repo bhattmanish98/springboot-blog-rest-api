813201cd563ca8521cb8ecf66b31a873963a8049
Manish Bhatt <manish.bhatt@lji.io>
2022-02-17 23:35:30 +0530

    added logic to insert meta data in the tables

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,12 +1,16 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
-public class SpringBootBlogRestApiApplication {
+public class SpringBootBlogRestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -16,4 +20,17 @@ public class SpringBootBlogRestApiApplication {
 		SpringApplication.run(SpringBootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setRoleName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setRoleName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
